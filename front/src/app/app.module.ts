234247ed1bcaba456c4f149e1f567c2f92be91d2
correakarl <karl.correa.88@gmail.com>
2023-04-15 08:23:22 -0400

    Update any changes and setting with angular 15

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { SeriesListComponent } from './components/series-list/series-list.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
 import { SerieModalComponent } from './components/serie-modal/serie-modal.component';
--- front/src/app/components/series-list/series-list.component.ts
+++ front/src/app/components/series-list/series-list.component.ts
@@ -1,7 +1,7 @@
 import { SerieService } from './../../services/serie.service';
 import { Component, OnInit } from '@angular/core';
 import { Serie } from 'src/app/common/serie';
-import { UntypedFormBuilder, UntypedFormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-series-list',
@@ -12,7 +12,7 @@ export class SeriesListComponent implements OnInit {
 
   series: Serie[] = [];
 
-  formSerie: UntypedFormGroup = this.formBuilder.group({
+  formSerie: FormGroup = this.formBuilder.group({
     _id: [''],
     __v: [0],
     title: [''],
@@ -22,14 +22,14 @@ export class SeriesListComponent implements OnInit {
     resume: ['']
   });
 
-  category: UntypedFormGroup = this.formBuilder.group({
+  category: FormGroup = this.formBuilder.group({
     _id: [''],
     __v: [0],
     catName: [''],
     catImg: []
   });
 
-  gallery: UntypedFormGroup = this.formBuilder.group({
+  gallery: FormGroup = this.formBuilder.group({
     img: [''],
     path: ['']
   });
@@ -38,7 +38,7 @@ export class SeriesListComponent implements OnInit {
 
   constructor(
     private serieService: SerieService,
-    private formBuilder: UntypedFormBuilder
+    private formBuilder: FormBuilder
   ) {
   }
 
